2d8af826121bb765091ffa7babe6d4a8455c8e0c
Victor Denissen <vdenisse@student.s19.be>
2023-10-18 13:22:36 +0200

    header update for ft_free_char_array

--- header/libft.h
+++ header/libft.h
@@ -6,7 +6,7 @@
 /*   By: vdenisse <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/06 16:43:44 by vdenisse          #+#    #+#             */
-/*   Updated: 2023/10/17 11:31:29 by vdenisse         ###   ########.fr       */
+/*   Updated: 2023/10/18 13:22:21 by vdenisse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,6 +57,7 @@ void				ft_putchar_fd(char c, int fd);
 void				ft_putstr_fd(char *s, int fd);
 void				ft_putendl_fd(char *s, int fd);
 void				ft_putnbr_fd(int n, int fd);
+void				ft_free_char_array(char **array);
 
 t_list				*ft_lstnew(int content);
 t_list				*ft_lstlast(t_list *lst);
