c9f4a19423f7e3f26fdd234338bba93386d214f7
Ratchet <ratchet@s19.be>
2025-05-02 13:12:24 +0200

    strcmp

--- header/libft.h
+++ header/libft.h
@@ -48,6 +48,7 @@ char				*ft_strchr(const char *s, int c);
 int                 ft_starts_with(const char *str, const char *prefix);
 char				*ft_strrchr(const char *s, int c);
 int					ft_strncmp(const char *s1, const char *s2, size_t n);
+int ft_strcmp(const char *s1, const char *s2);
 void				*ft_memchr(const void *s, int c, size_t n);
 int					ft_memcmp(const void *s1, const void *s2, size_t n);
 char				*ft_strnstr(const char *big, const char *little,
